23e8ca2433c199b0a4096cc4d0a8616f893c79cc
petermulligan88 <aezur20@gmail.com>
2021-12-20 02:11:16 +0000

    Removed unused types.

--- app/src/types/index.d.ts
+++ app/src/types/index.d.ts
@@ -1,18 +1,4 @@
 /* eslint-disable */
-interface ApiError {
-  name?: string,
-  message?: string,
-  response?: {
-    data?: {
-      errors?: {},
-    },
-    status?: {},
-    headers?: {},
-  },
-  config?: {
-    url?: string,
-  },
-};
 
 interface User {
   name: string,
@@ -21,28 +7,17 @@ interface User {
   isAdmin?: boolean,
 };
 
-interface Credentials {
-  email: string,
-  password: string,
-};
-
-interface CredentialsWithErrors extends Credentials {
-  errors?: any;
-};
-
-interface EmailWithErrors {
-  email: string;
-  errors?: any;
-};
-
-interface RegistrationObject extends CredentialsWithErrors {
-  name: string;
-  password_confirmation: string;
-};
-
 interface Context {
   to: RouteLocationNormalized;
   from: RouteLocationNormalized;
   next: NavigationGuardNext;
   store: Store<AuthState>;
 };
+
+interface Middleware {
+  (
+    to: RouteLocationNormalized,
+    next: NavigationGuardNext,
+    store: Store<AuthState>
+  ): void;
+}
