7106e6af616bad3d5ac92c72435a76b704ee3f02
petermulligan88 <aezur20@gmail.com>
2021-12-20 06:16:25 +0000

    Added base input.

---
+++ app/src/components/input/BaseInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <label v-if="$attrs.label" :for="$attrs.id">{{ $attrs.label }}</label>
+  <input
+    :id="$attrs.id"
+    :type="$attrs.type"
+    :placeholder="$attrs.placeholder"
+    v-model="inputValue"
+  />
+  <div v-if="$attrs.errors">
+    <small v-for="error in $attrs.errors" :key="error">
+      {{error}}
+    </small>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+export default defineComponent({
+  props: {
+    modelValue: String,
+  },
+  data() {
+    return {
+      inputValue: this.modelValue,
+    };
+  },
+  emits: ['update:modelValue'],
+  watch: {
+    inputValue() {
+      this.$emit('update:modelValue', this.inputValue);
+    },
+  },
+});
+</script>
