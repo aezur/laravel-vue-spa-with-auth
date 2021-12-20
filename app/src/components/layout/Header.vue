edb1bf863d0c8b5cdf8df91ab13c6920dd3870fd
petermulligan88 <aezur20@gmail.com>
2021-12-20 02:27:34 +0000

    Updated 'to' param after route update.

--- app/src/components/layout/Header.vue
+++ app/src/components/layout/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <div v-if="loggedIn">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/home">Home</router-link> |
       <router-link to="/about">About</router-link>
       <LogoutButton />
     </div>
