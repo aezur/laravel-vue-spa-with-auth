5667f0216b33f3519fda64e1ddfe73f401673269
petermulligan88 <aezur20@gmail.com>
2021-12-25 03:33:31 +0000

    Added Links and Meta types for pagination.

--- app/src/vuex-shim.d.ts
+++ app/src/vuex-shim.d.ts
@@ -15,6 +15,8 @@ declare module '@vue/runtime-core' {
 
   interface AdminState {
     users?: User[],
+    meta?: Meta,
+    links?: Links,
   }
 
   interface UIState {
@@ -22,6 +24,23 @@ declare module '@vue/runtime-core' {
     loading?: boolean,
     error?: any,
     message?: string,
+  }
+
+  interface Links {
+    first?: string,
+    last?: string,
+    prev?: string,
+    next?: string,
+  }
+
+  interface Meta {
+    current_page: number,
+    from: number,
+    last_page: number,
+    path: string,
+    per_page: number,
+    to: number,
+    total: number,
   }
 
 
