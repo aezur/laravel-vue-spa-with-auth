3465174fd8a6a56c74f098f10fa823dd27312af4
petermulligan88 <aezur20@gmail.com>
2021-12-25 15:17:51 +0000

    Added recommded vue settings.

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,18 +1,33 @@
 module.exports = {
+  plugins: ['@typescript-eslint'],
   root: true,
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended'
-  ],
+  // Prerequisite `eslint-plugin-vue`, being extended, sets
+  // root property `parser` to `'vue-eslint-parser'`, which, for code parsing,
+  // in turn delegates to the parser, specified in `parserOptions.parser`:
+  // https://github.com/vuejs/eslint-plugin-vue#what-is-the-use-the-latest-vue-eslint-parser-error
   parserOptions: {
-    ecmaVersion: 2020
+    parser: require.resolve('@typescript-eslint/parser'),
+    extraFileExtensions: ['.vue'],
+    ecmaFeatures: {
+      jsx: true
+    }
   },
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended'
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+  },
+  overrides: [{
+    files: ['*.ts', '*.tsx'],
+    rules: {
+      // The core 'no-unused-vars' rules (in the eslint:recommeded ruleset)
+      // does not work with type definitions
+      'no-unused-vars': 'off',
+    }
+  }]
 }
