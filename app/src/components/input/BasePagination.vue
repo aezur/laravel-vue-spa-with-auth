aeefe45fef49b8c0ad0146a2eb33a9138bc1b86d
petermulligan88 <aezur20@gmail.com>
2021-12-25 11:09:03 +0000

    Fixed ts issues.

--- app/src/components/input/BasePagination.vue
+++ app/src/components/input/BasePagination.vue
@@ -26,13 +26,25 @@ import { defineComponent, computed } from 'vue';
 import { useRoute } from 'vue-router';
 export default defineComponent({
   props: {
-    meta: Object,
-    links: Object,
-    action: String,
-    path: String,
+    meta: {
+      type: Object,
+      required: true,
+    },
+    links: {
+      type: Object,
+      required: true,
+    },
+    action: {
+      type: String,
+      required: true,
+    },
+    path: {
+      type: String,
+      required: true,
+    },
   },
   methods: {
-    async changePage(page: string | number): void {
+    async changePage(page: string | number): Promise<void> {
       const url = typeof page === 'string' ?
         page : `${this.meta.path}?page=${page}`;
       await this.$store.dispatch(
@@ -41,20 +53,20 @@ export default defineComponent({
         if (this.path) {
           this.$router.push({
             path: this.path,
-            query: { page: this.meta.current_page },
+            query: { page: this.meta?.current_page || 1 },
           });
         }
       });
     },
   },
   watch: {
-    async currentPage(newVal: number, oldVal: number): void {
-      if (newVal > this.meta.last_page || newVal < 1) {
+    async currentPage(newVal: number): Promise<void> {
+      if (newVal > this.meta?.last_page || newVal < 1) {
         this.$router.push({
-          query: { page: this.meta.current_page },
+          query: { page: this.meta?.current_page },
         });
       }
-      else if (this.meta.current_page !== newVal) {
+      else if (this.meta?.current_page !== newVal) {
         await this.changePage(newVal);
       }
     }
@@ -62,7 +74,13 @@ export default defineComponent({
   setup() {
     const route = useRoute();
     return {
-      currentPage: computed(() => +route.query.page)
+      currentPage: computed(() => {
+        const page = route?.query?.page;
+        if (typeof page === 'string') {
+          return Number.parseInt(page);
+        }
+        return 1;
+      }),
     }
   }
 })
