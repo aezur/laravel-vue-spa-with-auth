2284b0b860b05c370817f0ee1cc015b977962314
petermulligan88 <aezur20@gmail.com>
2021-12-25 04:43:38 +0000

    Added some styling.

--- app/src/views/Users.vue
+++ app/src/views/Users.vue
@@ -1,14 +1,27 @@
 <template>
-  <div id="users"
+<div class="users">
+  <div class="user"
     v-for="user in $store.getters['admin/users']"
     :key="user">
-    <p>{{ user }}</p>
+    <div>
+      <label for="name"><b>Name: </b></label>
+      <p id="name">{{ user.name }}</p>
+      <label for="email"><b>Email: </b></label>
+      <p id="email">{{ user.email }}</p>
+    </div>
+    <div>      
+      <label for="name"><b>Verified: </b></label>
+      <p id="verified">{{ parseTimestamp(user.verified) }}</p>
+      <label for="admin"><b>Admin: </b></label>
+      <p id="admin">{{ user.isAdmin }}</p>
+    </div>
   </div>
   <BasePagination
     path="/users"
     :meta="$store.getters['admin/meta']"
     :links="$store.getters['admin/links']"
     action="admin/paginateUsers" />
+</div>
 </template>
 
 <script lang="ts">
@@ -16,5 +29,33 @@ import { defineComponent } from 'vue';
 import BasePagination from '@/components/input/BasePagination.vue';
 export default defineComponent({
   components: { BasePagination },
+  methods: {
+    parseTimestamp(ts: string) {
+      return new Date(ts).toLocaleString() || '-';
+    },
+  },
 });
 </script>
+
+<style lang="scss">
+@import '@/assets/scss/vars.scss';
+.users {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .user {
+    padding: .5rem 1rem;
+    display:grid;
+    grid-template-columns: 200px 200px;
+    grid-row: auto auto;
+    grid-column-gap: 20px;
+    grid-row-gap: 20px;
+    margin-bottom: 2rem;
+    border: 1px solid $fontColor;
+    p {
+      margin: 0;
+    }
+  }
+}
+</style>
