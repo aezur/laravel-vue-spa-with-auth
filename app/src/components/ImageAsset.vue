1a5ca5c910e084dca8f153829cf4fefa3b7ab062
petermulligan88 <aezur20@gmail.com>
2021-12-26 23:00:52 +0000

    Added wrapper for displaying images from assets server.

---
+++ app/src/components/ImageAsset.vue
@@ -0,0 +1,19 @@
+<template>
+  <img :src="url">
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  props: {
+    path: {
+      type: String,
+      default: '',
+    }
+  },
+  computed: {
+    url() {
+      return `${process.env.VUE_APP_ASSET_URL}${this.path}`;
+    },
+  },
+});
+</script>
