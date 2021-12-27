cd933367625bb5b5f31d00128f58330708b4524d
petermulligan88 <aezur20@gmail.com>
2021-12-27 22:12:59 +0000

    Added base button for easier styling.

---
+++ app/src/components/input/BaseButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button>
+    <slot />
+  </button>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  props: {
+    text: {
+      type: String,
+      default: () => '',
+    }
+  }
+});
+</script>
+<style lang="scss" scoped>
+@import '@/assets/scss/colors';
+button {
+  color: $link;
+  background-color: $primary;
+  cursor: pointer;
+  font-size: 1.1rem;
+  padding: .375rem .75rem;
+  text-transform: uppercase;
+  border: none;
+  border-radius: .25rem;
+  &:hover,:focus {
+    background-color: $primary-light;
+  }
+  &:active {
+    background-color: $primary-dark;
+  }
+}
+</style>
