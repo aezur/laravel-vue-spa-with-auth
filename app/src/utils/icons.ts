1cba1fad66bc434a557ffcbfe96526cece763e3c
petermulligan88 <aezur20@gmail.com>
2021-12-27 20:21:37 +0000

    Added font awesome icons.

---
+++ app/src/utils/icons.ts
@@ -0,0 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faBars,
+  faTimes,
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+  faTimes,
+  faBars,
+);
+
+export default FontAwesomeIcon;
