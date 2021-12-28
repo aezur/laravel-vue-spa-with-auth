2d9a5590d17419d956da78928c73784233480b65
petermulligan88 <aezur20@gmail.com>
2021-12-28 13:18:04 +0000

    Added a plain link component.

---
+++ app/src/components/links/Link.vue
@@ -0,0 +1,32 @@
+<template>
+  <router-link :to="path">
+    {{ text }}
+  </router-link>
+</template>
+<script lang="ts">
+import { defineComponent, computed } from 'vue';
+import { useStore } from 'vuex';
+export default defineComponent({
+  props: {
+    text: {
+      type: String,
+      required: true,
+      default: () => "",
+    },
+    path: {
+      type: String,
+      required: true,
+      default: () => "",
+    },
+  },
+});
+</script>
+<style lang="scss" scoped>
+@import '@/assets/scss/colors';
+a {
+  color: $secondary;
+  &:hover,:focus,:active {
+    color: $primary;
+  }
+}
+</style>
